affb0aa7783785181f252446630704a0baa0c52c
Caio Henrique <caio.henrique.20111@gmail.com>
2021-01-30 12:51:22 -0300

    Outras entidades e segunda migration

--- Migrations/20210130154552_OtherEntities.Designer.cs
+++ Migrations/20210130154552_OtherEntities.Designer.cs
@@ -9,7 +9,7 @@ using SalesWebMvc.Data;
 namespace SalesWebMvc.Migrations
 {
     [DbContext(typeof(SalesWebMvcContext))]
-    [Migration("20210130104932_OtherEntities")]
+    [Migration("20210130154552_OtherEntities")]
     partial class OtherEntities
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -48,7 +48,7 @@ namespace SalesWebMvc.Migrations
 
                     b.HasIndex("SellerId");
 
-                    b.ToTable("SalesRecords");
+                    b.ToTable("SalesRecord");
                 });
 
             modelBuilder.Entity("SalesWebMvc.Models.Seller", b =>
